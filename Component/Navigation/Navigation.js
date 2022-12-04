f465bc887acf998614c65fc0598e490d31ef2c1d
dirga89 <dirga89@gmail.com>
2022-12-05 04:19:23 +0700

    Updates

---
+++ Component/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () =>
+{
+    return(
+        <nav className="tr f3 dim black pointer underline pa3">
+            <p>Sign Out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
